07bf44fffdca7e6ae4e7968596a7a365c77d0d72
Dana-Ar <DANA.ARCE@ucr.ac.cr>
2024-10-16 20:17:16 -0600

    Cargar proyecto a repositorio

---
+++ src/main/java/ucr/ac/cr/tm2100/g3/game/Game.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.tm2100.g3.game;
+
+import ucr.ac.cr.tm2100.g3.game.controller.ControllerGame;
+
+/**
+ *
+ * @author danar
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        ControllerGame controllerGame = new ControllerGame();
+    }
+}
---
+++ src/main/java/ucr/ac/cr/tm2100/g3/game/controller/ControllerGame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucr.ac.cr.tm2100.g3.game.controller;
+
+import ucr.ac.cr.tm2100.g3.game.view.GUIFrameMenu;
+
+/**
+ *
+ * @author danar
+ */
+public class ControllerGame {
+    private GUIFrameMenu guiFrameG;
+
+    public ControllerGame() {
+        guiFrameG = new GUIFrameMenu();
+        guiFrameG.setVisible(true);
+    }
+    
+    
+}
